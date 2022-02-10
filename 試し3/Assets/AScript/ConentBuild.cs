49821f5eeb5a8d151f80c859360fb77c145a2f90
yui-furuta <56290250+yui-furuta@users.noreply.github.com>
2022-02-11 00:48:01 +0900

    セーブ版とビルド版
    buttonがビルド版
    button１がセーブ版

--- 試し3/Assets/AScript/ConentBuild.cs
+++ 試し3/Assets/AScript/ConentBuild.cs
@@ -1,18 +1,80 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
+using UnityEngine.UI;
 
 public class ConentBuild : MonoBehaviour
 {
+    [SerializeField]
+	RectTransform prefab = null;
+    [SerializeField] GameObject ListPanel;
+
+    [SerializeField]
+    private GameObject ParentObject;
+    private GameObject[] ChildObject;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        // var item = GameObject.Instantiate(prefab) as RectTransform;
+		// item.SetParent(transform, false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(ListPanel.activeSelf == true){
+           contentLoad();
+        }
+    }
+
+    public void contentLoad(){
+       // SaveData saveData = Load();
+        Debug.Log(string.Join(",", inputBuild.testList));
+    }
+
+    public void pushContentButton(){
+       // SaveData saveData = Load();
+        for(int i=0; i<inputBuild.testList.Count; i++){
+        string v = inputBuild.testList[i];
+        //Debug.Log(v);
+        var item = GameObject.Instantiate(prefab) as RectTransform;
+		item.SetParent(transform, false);
+
+        var text = item.GetComponentInChildren<Text>();
+	    text.text = v;
+        }
+    }
+
+    public void pushLoadContentButton(){
+        //SaveData saveData = Load();
+        inputBuild.testList.Clear();
+
+        ChildObject = new GameObject[ParentObject.transform.childCount];
+
+        for (int i = 0; i < ParentObject.transform.childCount; i++)
+        {
+            ChildObject[i] = ParentObject.transform.GetChild(i).gameObject;
+            var text = ChildObject[i].GetComponentInChildren<Text>();
+            inputBuild.testList.Add(text.text);
+        }
+        Debug.Log(string.Join(",", inputBuild.testList));
+        //Save(saveData);
+         foreach (Transform child in gameObject.transform)
+        {
+            //削除する
+            Destroy(child.gameObject);
+        }
+
+        // for(int i=0; i<saveData.tamaList.Count; i++){
+        // string v = saveData.tamaList[i];
+        // Debug.Log(v);
+        // var item = GameObject.Instantiate(prefab) as RectTransform;
+		// item.SetParent(transform, false);
+
+        // var text = item.GetComponentInChildren<Text>();
+	    // text.text = v;
+        //}
     }
 }
---
+++ 試し3/Assets/AScript/wallChange.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using UnityEngine.UI;
+
+public class wallChange : MonoBehaviour
+{
+
+    [SerializeField] GameObject pink;
+    [SerializeField] GameObject green;
+    [SerializeField] GameObject yellow;
+    [SerializeField] GameObject blue;
+    [SerializeField] GameObject orange;
+    [SerializeField] GameObject gray;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pushPink();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void pushPink(){
+        pink.SetActive(true);
+        green.SetActive(false);
+        yellow.SetActive(false);
+        blue.SetActive(false);
+        orange.SetActive(false);
+        gray.SetActive(false);
+    }
+
+    public void pushGreen(){
+        pink.SetActive(false);
+        green.SetActive(true);
+        yellow.SetActive(false);
+        blue.SetActive(false);
+        orange.SetActive(false);
+        gray.SetActive(false);
+    }
+
+    public void pushYellow(){
+        pink.SetActive(false);
+        green.SetActive(false);
+        yellow.SetActive(true);
+        blue.SetActive(false);
+        orange.SetActive(false);
+        gray.SetActive(false);
+    }
+
+    public void pushBlue(){
+        pink.SetActive(false);
+        green.SetActive(false);
+        yellow.SetActive(false);
+        blue.SetActive(true);
+        orange.SetActive(false);
+        gray.SetActive(false);
+    }
+
+    public void pushOrange(){
+        pink.SetActive(false);
+        green.SetActive(false);
+        yellow.SetActive(false);
+        blue.SetActive(false);
+        orange.SetActive(true);
+        gray.SetActive(false);
+    }
+
+    public void pushGray(){
+        pink.SetActive(false);
+        green.SetActive(false);
+        yellow.SetActive(false);
+        blue.SetActive(false);
+        orange.SetActive(false);
+        gray.SetActive(true);
+    }
+}
